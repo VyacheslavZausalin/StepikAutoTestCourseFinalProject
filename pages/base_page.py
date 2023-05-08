2ffbd0faf1ee9141e60109773e36eea3c262670d
vaceslavzausalin <zausalin90@mail.ru>
2023-05-08 18:05:24 +0300

    добавление проверки эдемента на странице

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,22 @@
+from selenium.common.exceptions import NoSuchElementException
 # Создаем базовую страницу, от которой будут унаследованы все остальные классы.
 # В ней мы опишем вспомогательные методы для работы с драйвером.
 class BasePage():
     # добавим конструктор — метод, который вызывается, когда мы создаем объект. Конструктор объявляется
     # ключевым словом __init__. В него в качестве параметров мы передаем экземпляр драйвера и url адрес.
     # Внутри конструктора сохраняем эти данные как аттрибуты нашего класса.
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout) #добавим команду для неявного ожидания со значением по умолчанию в 10
 
     # добавим еще один метод open. Он должен открывать нужную страницу в браузере, используя метод get().
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
