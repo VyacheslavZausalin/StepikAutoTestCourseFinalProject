50b07a48357ec5a9baf47724fd4fb62783693556
vaceslavzausalin <zausalin90@mail.ru>
2023-05-10 20:29:36 +0300

    переход на страницу логина перенесен в base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
+from .locators import BasePageLocators
 import math
 
 
@@ -65,3 +66,10 @@ class BasePage():
             return True
 
         return False
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
